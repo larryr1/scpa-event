fafa37f2bd84b73324865d569c89c02fa1309714
Larry Rowe <26939860+larryr1@users.noreply.github.com>
2024-07-24 18:35:00 -0400

    Add version indicator

--- client-display/src/App.js
+++ client-display/src/App.js
@@ -4,17 +4,22 @@ import { Clock } from './components/utilities/Clock';
 import { EventPreview } from './components/EventPreview';
 import { Messages } from './components/Messages';
 
+import packageJson from '../package.json';
+
 function App() {
 
   console.log("Rendering app!")
 
   return (
     <div>
-      <p className='attribution'>This display programmed by Larry Rowe, SCPA IT Department.<br />{window.location.href}<br />{process.env.NODE_ENV.toUpperCase()} MODE</p>
+      <p className='attribution'>
+        This display programmed by Larry Rowe, SCPA IT Department.<br />
+        {window.location.href}<br />
+        {process.env.NODE_ENV.toUpperCase()} MODE — {packageJson.human_version}</p>
       <div className="App">
         <img src={logo} className='App-logo' alt='SCPA Logo' />
         <div className="App-body">
-          <h1 className='School-color fw-bold display-2'>Somerset College Preparatory Academy</h1>
+          <h1 className='School-color fw-bold display-2 mb-3'>Somerset College Preparatory Academy</h1>
           <Clock className="App-time" />
           <EventPreview className="mt-5" />
           <Messages />
